c343bb92da27c43261f20229a184f851f1a0aecd
Limon <limon.3cs@gmail.com>
2023-04-10 08:30:38 +0600

    initial

---
+++ src/Data.java
@@ -0,0 +1,17 @@
+
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+public class Data implements Serializable,Cloneable{
+
+    public String message;
+
+    public Object clone()throws CloneNotSupportedException{
+        return super.clone();
+    }
+}
+  
